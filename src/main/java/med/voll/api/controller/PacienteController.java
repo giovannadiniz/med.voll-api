109bebfe9a9ea9c4e0503f27cbe6b73ef826f71a
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2025-04-02 09:28:53 -0300

    first commit

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,12 @@
+package med.voll.api.controller;
+
+import med.voll.api.paciente.DadosCadastroPaciente;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class PacienteController {
+
+    public void cadastrarPaciente(@RequestBody DadosCadastroPaciente dadosPaciente){
+
+    }
+
+}
