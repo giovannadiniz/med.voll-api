dd4627d8d69f9466ae87608ce2f958e969d04a28
giovanna.diniz1 <giovanna.diniz@placeti.com.br>
2025-05-05 09:02:07 -0300

    feature: trying to use docker in the project

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,12 @@
 package med.voll.api.controller;
 
 import med.voll.api.paciente.DadosCadastroPaciente;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class PacienteController {
 
+    @PostMapping
     public void cadastrarPaciente(@RequestBody DadosCadastroPaciente dadosPaciente){
 
     }
